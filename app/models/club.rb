e083879ca21cb7df826fc6ca89a5fb3e17f6764e
SallyZ12 <szasloff@gmail.com>
2019-07-21 12:45:12 -0400

    added associations and validations

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,11 @@
 class Club < ApplicationRecord
+
+  has_many :courts
+  has_many :reservations
+  has_many :players, through: :reservations
+
+  validates :club_name, presence: true
+  validates :location, presence: true
+  validates :nunmber_courts, presence: true
+
 end
