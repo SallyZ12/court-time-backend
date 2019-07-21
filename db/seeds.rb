90272bf5ec571e0f0675437c3088df91ea78b53d
SallyZ12 <szasloff@gmail.com>
2019-07-21 14:35:03 -0400

    dropped db, migrated and re-seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(id: 1, username: 'Roger', email: "roger@email.come", password: "password", admin: "No")
+
+Club.create(id:1, club_name: "West Side", location: "NY", number_courts: 2)
+
+Court.create(id: 1, club_id: 1, court_number: 1, surface: 'grass')
+Court.create(id: 2, club_id: 1, court_number: 2, surface: 'grass')
+
+Rate.create(id:1, club_id: 1, description: 'prime', rate: 60)
+Rate.create(id:2, club_id: 1, description: 'non-prime', rate: 50)
+
+
+Reservation.create(id: 1, user_id: 1, court_id: 1, rate_id: 1, hour: 8, day: 07/21/2019)
