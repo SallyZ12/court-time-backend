9e718e829be1d377a5b0474ceb035f533cb68aa3
SallyZ12 <szasloff@gmail.com>
2019-07-21 12:26:55 -0400

    namespaced the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :courts
-  resources :rates
-  resources :clubs
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+     namespace :v1 do
+       resources :reservations
+       resources :courts
+       resources :rates
+       resources :clubs
+       resources :users
+     end
+  end
+
+  
 end
