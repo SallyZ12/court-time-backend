da4ff5a097bf798d8ead7a3ded9779cf50f9cace
SallyZ12 <szasloff@gmail.com>
2019-07-21 12:54:36 -0400

    added associations and validations

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,2 +1,10 @@
 class Court < ApplicationRecord
+
+  belongs_to :club
+  has_many :reservations
+  has_many :users, through: :reservations
+
+  validates: surface, presence: true
+  validates: court_number, presence: true
+
 end
