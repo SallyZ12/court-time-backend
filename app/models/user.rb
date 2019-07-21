2791141c8e88b1ab037f458f63a3de0a9ad4141b
SallyZ12 <szasloff@gmail.com>
2019-07-21 13:15:49 -0400

    added associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  has_many :reservations
+  has_many :club, through: :reservations
+  has_many :courts, through: :reservations
+
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+
+  validates :password, presence: true, length: (minimum: 8)
+
+  validates_inclusion_of :admin, :in => ["Yes", "No"]
+
 end
