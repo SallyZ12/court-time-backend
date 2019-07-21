055a680928e1f8ed7f446bcc36bce6cd3296d729
SallyZ12 <szasloff@gmail.com>
2019-07-21 13:22:21 -0400

    added associations and validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,10 @@
 class Reservation < ApplicationRecord
+
+  belongs_to :court
+  belongs_to :user
+  belongs_to :rate
+  belongs_to :club, through: :courts
+
+  validates :hour, :day, presence: true
+
 end
