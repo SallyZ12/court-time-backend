c73d7fb155323a82404173891f4434b6f687ae27
SallyZ12 <szasloff@gmail.com>
2019-07-22 18:34:58 -0400

    added belongs_to :rate

--- app/models/club.rb
+++ app/models/club.rb
@@ -3,6 +3,7 @@ class Club < ApplicationRecord
   has_many :courts
   has_many :reservations
   has_many :players, through: :reservations
+  belongs_to :rate
 
   validates :club_name, presence: true
   validates :location, presence: true
