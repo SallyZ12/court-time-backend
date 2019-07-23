72c6786ec65dee155181a3c714ead487be319a42
SallyZ12 <szasloff@gmail.com>
2019-07-23 10:26:41 -0400

    changed association to has_many :club from clubs

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,7 @@ class Reservation < ApplicationRecord
   belongs_to :court
   belongs_to :user
   belongs_to :rate
-  has_many :clubs, through: :court
+  has_many :club, through: :courts
 
   validates :hour, :day, presence: true
 
