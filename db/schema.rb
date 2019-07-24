d4558aa7b154fef2618d55f8e22403680d2d110c
SallyZ12 <szasloff@gmail.com>
2019-07-24 14:33:34 -0400

    added new column confirmID to reservations table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_134808) do
+ActiveRecord::Schema.define(version: 2019_07_24_183217) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "club_name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_134808) do
     t.date "day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmID"
   end
 
   create_table "users", force: :cascade do |t|
