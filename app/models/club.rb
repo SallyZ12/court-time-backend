cdb6860bb57ae26b728f9d2f018ede0dd609edcc
SallyZ12 <szasloff@gmail.com>
2019-07-25 17:01:45 -0400

    deleted associations to rates since removed this table/model

--- app/models/club.rb
+++ app/models/club.rb
@@ -3,7 +3,6 @@ class Club < ApplicationRecord
   has_many :courts
   has_many :reservations, through: :courts
   has_many :players, through: :reservations
-  has_many :rates
 
   validates :club_name, presence: true
   validates :location, presence: true
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,6 @@ class Reservation < ApplicationRecord
 
   belongs_to :court
   belongs_to :user
-  belongs_to :rate
   has_many :club, through: :courts
 
   validates :hour, :day, presence: true
