23b2cb969d680f5f98fd0e93cd8b3551e19b0f70
SallyZ12 <szasloff@gmail.com>
2019-07-25 19:13:05 -0400

    corrected has_many: to users from :players

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,7 +2,7 @@ class Club < ApplicationRecord
 
   has_many :courts
   has_many :reservations, through: :courts
-  has_many :players, through: :reservations
+  has_many :users, through: :reservations
 
   validates :club_name, presence: true
   validates :location, presence: true
