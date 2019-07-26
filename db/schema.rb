c323fc392ecba3aa1aab54c16d1c6fca838b673b
SallyZ12 <szasloff@gmail.com>
2019-07-26 08:08:16 -0400

    added new attribute rate to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_220945) do
+ActiveRecord::Schema.define(version: 2019_07_26_115845) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "club_name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_220945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "confirmID"
+    t.integer "rate"
   end
 
   create_table "users", force: :cascade do |t|
