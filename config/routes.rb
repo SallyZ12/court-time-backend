b7576d5b4903bd269cbc7db3b21f2fb6fc0670db
SallyZ12 <szasloff@gmail.com>
2019-07-27 13:50:37 -0400

    namespaced courts to clubs

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :reservations
-       resources :courts
-       resources :clubs
        resources :users
+
+       resources :clubs do
+         resources :courts
+       end
      end
   end
 
