f821d357e505b2603618fb6a925f646bf1d757ba
SallyZ12 <szasloff@gmail.com>
2019-07-27 18:14:18 -0400

    initial build of actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user =User.new(user_params)
+      if @user.save
+        render json: @user
+      else
+        render json: {error: 'Error creating user'}
+      end
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def set_user
+    @user = Club.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :admin)
+  end
+
 end
