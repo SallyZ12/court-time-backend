f1374845af84f5477001a5c9da0e8a8b99f80fbf
SallyZ12 <szasloff@gmail.com>
2019-07-27 14:17:17 -0400

    changed seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(id: 1, username: 'Roger', email: "roger@email.com", password: "password", admin: "No")
-User.create(id: 2, username: 'Venus', email: "venus@email.com", password: "password", admin: "No")
+user = User.create(username: 'Roger', email: "roger@email.com", password: "password", admin: "No")
+user2 = User.create(username: 'Venus', email: "venus@email.com", password: "password", admin: "No")
 
-Club.create(id: 1, club_name: "West Side", location: "NY", number_courts: 2)
-Club.create(id: 2, club_name: "Rock", location: "NJ", number_courts: 2)
+club = Club.create(club_name: "West Side", location: "NY", number_courts: 2)
+club2 = Club.create(club_name: "Rock", location: "NJ", number_courts: 2)
 
-Court.create(id: 1, club_id: 1, court_number: 1, surface: 'grass', prime: 60, non_prime: 50)
-Court.create(id: 2, club_id: 1, court_number: 2, surface: 'grass', prime: 60, non_prime: 50)
-Court.create(id: 3, club_id: 1, court_number: 3, surface: 'hard', prime: 55, non_prime: 45)
-Court.create(id: 4, club_id: 2, court_number: 2, surface: 'har-tru', prime: 50, non_prime: 40)
+court1 = Court.create(club_id: 1, court_number: 1, surface: 'grass', prime: 60, non_prime: 50)
+court2 = Court.create(club_id: 1, court_number: 2, surface: 'grass', prime: 60, non_prime: 50)
+court3 = Court.create(club_id: 1, court_number: 3, surface: 'hard', prime: 55, non_prime: 45)
+court4 = Court.create(club_id: 2, court_number: 2, surface: 'har-tru', prime: 50, non_prime: 40)
 
 
-Reservation.create(id: 1, user_id: 1, court_id: 1, hour: '12:00 pm', day: '2019-07-21', confirmID: 123456, rate_type: 'prime', rate:60 )
-Reservation.create(id: 2, user_id: 2, court_id: 3, hour: '8:00 am', day: '2019-07-22', confirmID: 234567, rate_type: 'prime', rate: 55)
-Reservation.create(id: 3, user_id: 1, court_id: 2, hour: '1:00 pm' , day: '2019-07-24', confirmID: 345678, rate_type: 'non-prime', rate: 50)
-Reservation.create(id: 4, user_id: 1, court_id: 4, hour: '4:00 pm', day: '2019-07-25', confirmID: 456789, rate_type: 'non-prime', rate: 40)
+reservation1 = Reservation.create(user_id: 1, court_id: 1, hour: '12:00 pm', day: '2019-07-21', confirmID: 123456, rate_type: 'prime', rate:60 )
+reservation2 = Reservation.create(user_id: 2, court_id: 3, hour: '8:00 am', day: '2019-07-22', confirmID: 234567, rate_type: 'prime', rate: 55)
+reservation3 = Reservation.create(user_id: 1, court_id: 2, hour: '1:00 pm' , day: '2019-07-24', confirmID: 345678, rate_type: 'non-prime', rate: 50)
+reservation4 = Reservation.create(user_id: 1, court_id: 4, hour: '4:00 pm', day: '2019-07-25', confirmID: 456789, rate_type: 'non-prime', rate: 40)
