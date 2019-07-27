a64b09ef72e69050d94e898fefa3aa455e1a4558
SallyZ12 <szasloff@gmail.com>
2019-07-27 13:44:54 -0400

    starting to build courts controller

--- app/controllers/api/v1/courts_controller.rb
+++ app/controllers/api/v1/courts_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::CourtsController < ApplicationController
+
+    before_action :set_club
+
+    def index
+      @courts = Court.all
+      render json: @courts
+    end
+
+    def show
+      @court = Court.find(params[:id])
+      render json: @court
+    end
+
+    def create
+      @court = @club.courts.new(court_params)
+      @court.save
+      render json: @court
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_club
+      @club = Club.find(params[:club_id])
+    end
+
+    def set_court
+      @court = Court.find(params[:id])
+    end
+
+    def court_params
+      params.require(:court).permit(:club_id, :court_number, :surface, :prime, :non_prime)
+    end
+
+
+
+
+
 end
