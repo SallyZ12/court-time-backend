e058faa8e450e0c89764d2d50b32ba653f3dcdd2
SallyZ12 <szasloff@gmail.com>
2019-07-27 18:45:12 -0400

    started build of reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::ReservationsController < ApplicationController
+
+  def index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    render json: @reservation
+  end
+
+
+  private
+
+  def set_reservations
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :court_id, :rate_type, :rate, :hour, :day, :confirmID)
+  end
+
+
+
 end
