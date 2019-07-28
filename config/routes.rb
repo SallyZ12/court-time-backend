9e8f30c66f7abe576f8b93959347db873ab7868c
SallyZ12 <szasloff@gmail.com>
2019-07-28 19:55:26 -0400

    resting routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,26 @@ Rails.application.routes.draw do
 
   namespace :api do
      namespace :v1 do
-       resources :reservations
+       # resources :reservations
        resources :users
        resources :clubs
 
-            resources :users do
-              resources :reservations
 
-            resources :courts do
-              resources :reservations
+        resources :users do
+            resources :reservations
 
-          end
-         end
+
+        # resources :clubs do
+        #   resources :courts
+
+
+          # resources :courts do
+          #   resources :reservations
+
         end
-       end
+      end
+    end
+  # end
+# end
+
 end
