e829aee2ae778af5b2d4e9e99ae1a69ef5be49e8
SallyZ12 <szasloff@gmail.com>
2019-07-29 18:48:36 -0400

    added methods for current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+   include ::ActionController::Cookies
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
