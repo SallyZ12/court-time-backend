f5e2d1534cb121aecbf48fba4e9b27e70ce767d3
SallyZ12 <szasloff@gmail.com>
2019-07-29 19:02:09 -0400

    building actions for  create, get_current_user, and destroy

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,35 @@
 class Api::V1::SessionsController < ApplicationController
 
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user, status: :ok
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      notice: "successfully logged out"
+    }, status: :ok
+  end
+
+
+
 end
