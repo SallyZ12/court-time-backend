6b0ad4eb49282e2c9efa6b1f4112ed920f003a10
SallyZ12 <szasloff@gmail.com>
2019-07-30 14:34:49 -0400

    added session info to create and some error responses

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,9 +14,13 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user =User.new(user_params)
       if @user.save
-        render json: @user
+          session[:user_id] = @user.id
+        render json: @user, status: :created
       else
-        render json: {error: 'Error creating user'}
+        resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
       end
   end
 
