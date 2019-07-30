8ee1160d6116862f50187d4d673fbeb92338125d
SallyZ12 <szasloff@gmail.com>
2019-07-30 08:41:12 -0400

    needed to specify specific urls for origins rather than * to work with credentials: true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
