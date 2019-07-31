9e566fd43ed0245c2b9a599a16e100e087ff6256
SallyZ12 <szasloff@gmail.com>
2019-07-31 08:43:53 -0400

    added first_name and last_name columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_115845) do
+ActiveRecord::Schema.define(version: 2019_07_31_124241) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "club_name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_07_26_115845) do
     t.string "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
