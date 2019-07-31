5fdd01ee3004185c77ac53866e47190aa4a4f739
SallyZ12 <szasloff@gmail.com>
2019-07-31 18:16:44 -0400

    added prime_time and non_prime_time methods to court model define which hours have prime time vs non_prime time rates and added also to serializer.

--- app/models/court.rb
+++ app/models/court.rb
@@ -7,4 +7,14 @@ class Court < ApplicationRecord
   validates :surface, presence: true
   validates :court_number, presence: true
 
+
+  def prime_time
+    return ['9:00 am', '10:00 am', '11:00 am', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm', '10:00 pm']
+  end
+
+  def non_prime_time
+    return ['12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm']
+  end
+
+
 end
--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,7 +1,7 @@
 class CourtSerializer < ActiveModel::Serializer
-  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime
+  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :prime_time, :non_prime_time
 
   belongs_to :club
   has_many  :reservations
-  
+
 end
