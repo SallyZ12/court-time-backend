02f4a1e69c96095b1fef888e4df966587ce4c630
SallyZ12 <szasloff@gmail.com>
2019-08-02 11:54:55 -0400

    fixed get_current_user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
-  get "api/v1/get_current_user", to: "api/v1/get_current_user"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
-  
+
   namespace :api do
      namespace :v1 do
        # resources :reservations
