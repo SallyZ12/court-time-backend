8ee35d48af7be698e045eeede19801e42a0808fa
SallyZ12 <szasloff@gmail.com>
2019-08-02 10:39:43 -0400

    added a delete route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
   get "api/v1/get_current_user", to: "api/v1/get_current_user"
-
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  
   namespace :api do
      namespace :v1 do
        # resources :reservations
