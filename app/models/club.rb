7432d08b49c54f545be51677ff8c7c7e0228265d
SallyZ12 <szasloff@gmail.com>
2019-08-12 16:09:27 -0400

    removed validation for number_courts since this column was deleted

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,11 +2,10 @@ class Club < ApplicationRecord
 
   has_many :courts
   has_many :reservations, through: :courts
-  
+
   has_many :users, through: :reservations
 
   validates :club_name, presence: true
   validates :location, presence: true
-  validates :number_courts, presence: true
-
+  
 end
