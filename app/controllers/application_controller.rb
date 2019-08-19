9df068ef028c7bd48bc74b29dff941c76e736059
SallyZ12 <szasloff@gmail.com>
2019-08-19 06:09:26 -0400

    reverted back to code prior to the last change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::API
    include ::ActionController::Cookies
 
-   helper_method :current_user
-   helper_method :logged_in?
+   # helper_method :current_user
+   # helper_method :logged_in?
+
 
    def current_user
-     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+     User.find_by(id: session[:user_id])
+   end
+
+   # def current_user
+   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   #  end
 
     def logged_in?
       !!current_user
@@ -14,7 +19,3 @@ class ApplicationController < ActionController::API
 
 
 end
-
-# def current_user
-#   User.find_by(id: session[:user_id])
-# end
