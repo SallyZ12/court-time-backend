e484d5278040a1460987da3cadd4bd8bff4758a3
SallyZ12 <szasloff@gmail.com>
2019-08-22 13:34:50 -0400

    modified permitted params, deleted userId and courtId, and changed index to include @user = current_user rather than @user = set_user

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::ReservationsController < ApplicationController
   def index
 
     if params[:user_id]
-      @user = set_user
+      @user = current_user
       @reservations = @user.reservations
       else
         @reservations = Reservation.all
@@ -59,7 +59,7 @@ class Api::V1::ReservationsController < ApplicationController
 
 
   def reservation_params
-    params.require(:reservation).permit(:user_id, :court_id, :rate_type, :rate, :hour, :day, :confirmID)
+    params.require(:reservation).permit(:rate_type, :rate, :hour, :day, :confirmID)
   end
 
 
