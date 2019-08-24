d8da5b476e3bc344b140d04f3cff84c4e9af1229
SallyZ12 <szasloff@gmail.com>
2019-08-24 14:36:36 -0400

    reduced hours in array and also prime and non_prime

--- app/models/court.rb
+++ app/models/court.rb
@@ -8,15 +8,15 @@ class Court < ApplicationRecord
   validates_uniqueness_of :court_number, scope: :club_id
 
   def hours
-    return ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm', '10:00 pm']
+    return ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm']
   end
 
   def prime_time
-    return ['9:00 am', '10:00 am', '11:00 am', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm', '10:00 pm']
+    return ['9:00 am', '10:00 am', '11:00 am']
   end
 
   def non_prime_time
-    return ['12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm']
+    return ['12:00 pm', '1:00 pm', '2:00 pm']
   end
 
 
