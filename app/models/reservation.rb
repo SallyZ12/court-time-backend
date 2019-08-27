8e14bb5f590fecac4dd471865682cc8f1c7f4e15
SallyZ12 <szasloff@gmail.com>
2019-08-27 15:25:57 -0400

    added validation so can't book a court on same time and day

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,10 @@ class Reservation < ApplicationRecord
 
   has_one :club, through: :court
 
+
+  validates :court_id, uniqueness: {scope: [:day, :hour]}
+
+
   # validates :hour, :day, presence: true
 
 end
