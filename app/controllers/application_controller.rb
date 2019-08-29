9d7c6bb015a337d58ce9b9f88dcdfb1290a45c91
SallyZ12 <szasloff@gmail.com>
2019-08-29 14:58:28 -0400

    went back to original current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::API
    include ::ActionController::Cookies
-   include ::ActionController::Helpers
+   # include ::ActionController::Helpers
 
-   helper_method :current_user
-   helper_method :logged_in?
+   # helper_method :current_user
+   # helper_method :logged_in?
 
 
    def current_user
      User.find_by(id: session[:user_id])
    end
+
   #  def current_user
-  #    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   # end
 
 
