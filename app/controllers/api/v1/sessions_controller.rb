c125f32306a199a34cbc65613048211fbb1d2007
SallyZ12 <szasloff@gmail.com>
2019-08-29 13:52:58 -0400

    tried some stuff for current_user, no changes made

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-  
+
     @user = User.find_by(username: params[:session][:username])
 
     if @user && @user.authenticate(params[:session][:password])
