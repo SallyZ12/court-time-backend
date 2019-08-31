c2c6bc1626f5fc00a33f6ac1f6c69b296bf202c0
SallyZ12 <szasloff@gmail.com>
2019-08-31 14:38:05 -0400

    deleted as attributes non_prime_time, prime_time and hours

--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,5 +1,5 @@
 class CourtSerializer < ActiveModel::Serializer
-  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :prime_time, :non_prime_time, :hours
+  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime
 
   belongs_to :club
   has_many  :reservations
