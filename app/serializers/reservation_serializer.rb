49976ab793903f11de6b33ecef7d3aa6a1d2970e
SallyZ12 <szasloff@gmail.com>
2019-08-31 14:41:27 -0400

    working on custom attribute for reservation club_name

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,7 +1,15 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :court_id, :rate_type, :rate, :hour, :day, :confirmID
+  attributes :id, :user_id, :court_id, :rate_type, :rate, :hour, :day, :confirmID, :reservation_club
+
 
   belongs_to :court
   belongs_to :user
+
+  def reservation_club
+    Reservation.all.select do |reservation|
+
+      return reservation.club.club_name
+    end
+  end
 
 end
