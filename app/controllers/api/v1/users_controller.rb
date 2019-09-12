4b6e63d259949af200807c60e8b02d907bcbb4fc
SallyZ12 <szasloff@gmail.com>
2019-09-12 14:05:16 -0400

    eliminated error renders for now to test edit function on front end

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -45,20 +45,15 @@ end
 
     @user =  set_user
     @user.update(user_params)
-        if params[:user][:admin] === 'No' || is_admin === true
 
-              if@user.save
+              if @user.save
                 render json: @user
-                resp = {
-                  error: @user.errors.full_messages.to_sentence
-                }
-                  render json: resp
+              #   resp = {
+              #     error: @user.errors.full_messages.to_sentence
+              #   }
+              #     render json: resp
               end
-        else
-          render json: {
-          error: "Admin exists -- only one permitted"
-          }
-        end
+
     end
 
 
