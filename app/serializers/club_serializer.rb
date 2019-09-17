208c363c4901c3075cd88e48731ca637b23511e4
SallyZ12 <szasloff@gmail.com>
2019-09-17 12:07:34 -0400

    added a custom attribute for club reservations

--- app/serializers/club_serializer.rb
+++ app/serializers/club_serializer.rb
@@ -1,5 +1,12 @@
 class ClubSerializer < ActiveModel::Serializer
-  attributes :id, :club_name, :location
+  attributes :id, :club_name, :location, :club_res
 
   has_many :courts
+
+
+  def club_res
+    self.object.reservations
+  end
+
+
 end
