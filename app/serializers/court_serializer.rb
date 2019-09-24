2d31ec5ac1bc1ebfc0ad2d942de494719bb93e7f
SallyZ12 <szasloff@gmail.com>
2019-09-24 13:06:37 -0400

    added a court_hours custom attribute

--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,11 +1,15 @@
 class CourtSerializer < ActiveModel::Serializer
-  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :court_res
+  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :court_res, :court_hours
 
   belongs_to :club
   has_many  :reservations
 
   def court_res
     self.object.reservations
+  end
+
+  def court_hours
+    self.object.hours
   end
 
 end
