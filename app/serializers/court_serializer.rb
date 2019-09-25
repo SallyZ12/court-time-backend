2d1fdb96f3ff1fdd5b4b65756ba38110e12374f7
SallyZ12 <szasloff@gmail.com>
2019-09-25 15:51:13 -0400

    deleted custome attribute court_hours

--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,15 +1,11 @@
 class CourtSerializer < ActiveModel::Serializer
-  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :court_res, :court_hours
+  attributes :id, :club_id, :court_number, :surface, :prime, :non_prime, :court_res
 
   belongs_to :club
   has_many  :reservations
 
   def court_res
     self.object.reservations
-  end
-
-  def court_hours
-    self.object.hours
   end
 
 end
