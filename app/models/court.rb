11f0314327ab556d716e35a18e3dbda473e6b1da
SallyZ12 <szasloff@gmail.com>
2019-09-30 17:53:00 -0400

    deleted methods not using for hours, prime and non-prime

--- app/models/court.rb
+++ app/models/court.rb
@@ -7,17 +7,6 @@ class Court < ApplicationRecord
   validates :court_number, :surface, :prime, :non_prime, presence: true
   validates_uniqueness_of :court_number, scope: :club_id
 
-  def hours
-    return ['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm']
-  end
-
-  def prime_time
-    return ['9:00 am', '10:00 am', '11:00 am']
-  end
-
-  def non_prime_time
-    return ['12:00 pm', '1:00 pm', '2:00 pm']
-  end
-
+  
 
 end
