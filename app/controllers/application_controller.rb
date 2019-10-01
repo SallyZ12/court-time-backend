672dd439fdb28b6ea9b81d71ffa0271ee99aa7a9
SallyZ12 <szasloff@gmail.com>
2019-10-01 14:30:40 -0400

    commented out is_admin method since only used in users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,25 +10,21 @@ class ApplicationController < ActionController::API
      User.find_by(id: session[:user_id])
    end
 
-  #  def current_user
-  #    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  # end
-
 
     def logged_in?
       !!current_player
     end
 
 
-    def is_admin
-      User.all.each do |user|
-        if user.admin === "Yes"
-          return false
-        end
-      end
-      return true
-    end
+    # def is_admin
+    #   User.all.each do |user|
+    #     if user.admin === "Yes"
+    #       return false
+    #     end
+    #   end
+    #   return true
+    # end
 
 
-  
+
 end
